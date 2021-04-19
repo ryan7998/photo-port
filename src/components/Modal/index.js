a960babc02fb80dc05c87f3dd342d1fdf621c882
fazle <ryan7998@gmail.com>
2021-04-18 22:45:46 -0400

    commiting again

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -7,7 +7,7 @@ function Modal({onClose, currentPhoto}){
         <div className="modalBackdrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">Photo Name</h3>
-                <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
+                <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
                 <p>{description}</p>
                 <button onClick={onClose} type="button">
                     Close this modal
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -114,7 +114,7 @@ function PhotoList({category}){
           <div className="flex-row">
             {currentPhotos.map((image, i) => (
               <img
-                src={require(`../../assets/small/${category}/${i}.jpg`).default}
+                src={require(`../../assets/small/${category}/${i}.jpg`)}
                 alt={image.name}
                 className="img-thumbnail mx-1"
                 onClick={()=>toggleModal(image, i)}
